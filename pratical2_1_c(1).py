79b9fb5b69b31f7a42550640c8aa436af366689f
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 14:33:22 +0530

    Add files via upload

---
+++ pratical2_1_c(1).py
@@ -0,0 +1,11 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to sum all items in a dictionary.
+Github link - https://github.com/Virti7702/Practical2-1-c.git
+
+'''
+a_dict = {1:1, 2:2, 3:3, 4:7}
+values = a_dict.values()
+total = sum(values)
+print(total)
